3e514632f0cf4e9281050eee4c1208c1ffdb22f0
PODonnell12 <PODonnell12@qub.ac.uk>
2022-01-29 00:57:16 +0000

    Nuke it all to death

--- Source/Moronavirus/Moronavirus.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Moronavirus.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Moronavirus, "Moronavirus" );
- 
